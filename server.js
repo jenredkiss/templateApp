f5c202397324d376637e59f9cbcbb919694aaf4f
Giovanni Brigoli <giovanni.brigoli@nokia.com>
2017-01-29 12:54:36 +0800

    modify to app

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 // Set our api routes
 app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'app/index.html'));
 });
 
 /**
